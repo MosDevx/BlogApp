61c5cf1504e959b60604efc73dbfb057fb3485bd
MosDevx <mosesk.wamae@gmail.com>
2023-05-06 14:51:08 +0300

    add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,9 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1, maximum: 20 }
   validates :text, presence: true, length: { minimum: 1, maximum: 1000 }
 
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 1, maximum: 20 }
   validates :bio, length: { minimum: 1, maximum: 100 }
 
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
