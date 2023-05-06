2a93e5b22b4128a873a060f77ddde3d7376a16f3
MosDevx <mosesk.wamae@gmail.com>
2023-05-06 11:28:42 +0300

    set up correct forign key author_id for model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', counter_cache: true
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
