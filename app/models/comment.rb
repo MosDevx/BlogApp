2051e8043abaa57489b9f49a3029783756dae2c7
MosDevx <mosesk.wamae@gmail.com>
2023-05-06 11:11:49 +0300

    change to models for User class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-	belongs_to :author , class_name: "User" , foreign_key: "user_id"
+	belongs_to :author , class_name: "User" 
 	belongs_to :post , counter_cache: true
 
 	validates :text , presence: true , length: {minimum: 1 , maximum: 1000}
