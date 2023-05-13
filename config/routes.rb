4558078c12e4ead0938262fbca11f441f3b1d0b2
MosDevx <mosesk.wamae@gmail.com>
2023-05-13 09:15:03 +0300

    Setup for users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
+
+
+  root 'users#index'
+
+  get '/users', to: "users#index"
+  get '/users/:id', to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
