baa6680f957d59272181c2956161db4664311ace
MosDevx <mosesk.wamae@gmail.com>
2023-05-13 15:46:04 +0300

    migrations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  # belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :user
   belongs_to :post, counter_cache: true
   # after_save :update_comments_counter
 
