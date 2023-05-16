0112c0c3edd5f1e2d904b5c4dd10ea865a472c02
MosDevx <mosesk.wamae@gmail.com>
2023-05-16 09:27:03 +0300

    fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
-    @my_name = ['A', 'B', 'C']
+    @my_name = %w[A B C]
     @user = User.find(params[:id])
     # @recent_posts = @user.recent_posts
     # @all_po:insts = @user.posts
