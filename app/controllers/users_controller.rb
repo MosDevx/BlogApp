706ff4e69d41f5ed7e316f7c4ecd14270c947160
MosDevx <mosesk.wamae@gmail.com>
2023-05-19 18:37:55 +0300

    add user tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
+    # @user = current_user
+    @user = User.find(params[:id])
   end
 
   def update
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,18 +2,31 @@ require 'rails_helper'
 
 RSpec.feature "Users", type: :feature , feat:true do
 
+  before(:all) do 
+    @user = User.create(name: 'Jann Doe', bio: 'I am John Doe',email:"rand_email")
+  end
+  
+  after(:all) do
+    @user.destroy
+  end
+  
   it 'displays all users' do
     visit users_path
     expect(page).to have_content('Users')
   end
-
+  
   it 'displays a user' do
-    user = User.new(name: 'John Doe', bio: 'I am John Doe')
-    user.save
-    visit user_path(user)
+    # user = User.create(name: 'John Doe', bio: 'I am John Doe')
+    # puts user.id
+    visit user_path(@user)
     expect(page).to have_content('John Doe')
   end
-
-
-
+  
+  it 'displays a user edit page' do
+    # user = User.create(name: 'John Doe', bio: 'I am John Doe')
+    visit edit_user_path(@user)
+    expect(page).to have_content('Edit User')
+  end
+  
+  
 end
